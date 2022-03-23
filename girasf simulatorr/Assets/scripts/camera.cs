6662ab9e75015c41e27a2db53ce06c8f130727c6
oskar ,ok <foldagerfamilie@gmail.com>
2022-03-23 17:26:27 +0100

    mat

--- girasf simulatorr/Assets/scripts/camera.cs
+++ girasf simulatorr/Assets/scripts/camera.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class camera_con : MonoBehaviour
+public class camera : MonoBehaviour
 {
+
+    public Transform player;
+    public Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        transform.position = player.position + offset;
     }
 }
