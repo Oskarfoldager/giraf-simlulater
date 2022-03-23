e58be16c3f14111afa8b700de4871075c034246e
oskar ,ok <foldagerfamilie@gmail.com>
2022-03-23 18:03:52 +0100

    giraf workers

--- girasf simulatorr/Assets/scripts/playerspawner.cs
+++ girasf simulatorr/Assets/scripts/playerspawner.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class playerspawner : MonoBehaviour
 {
     public GameObject player;
-
+    public float eds = 1;
 
     // Start is called before the first frame update
     void start()
     {
+
+    }
+    private void Update()
+    {
+        if (eds == 1)
+        {
             Debug.Log("Attempting to spawn player");
             GameObject thisproj = Instantiate(player, transform.position, transform.rotation);
+            eds = 2;
+        }
     }
 }
